db1240f3ed15081d0ff4555a709efebb5da1b8e7
Harsha Milinda <harshamilinda@gmail.com>
2018-02-01 00:45:54 +0800

    updates

--- src/app/cafe-dashboard/cafe-dashboard.component.ts
+++ src/app/cafe-dashboard/cafe-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Order, OrderDetails, Beverage } from '../model/cafe-model';
 import { CafeApiService } from '../services/cafe-api.service';
+import { HubConnection } from '@aspnet/signalr-client';
+
 
 @Component({
   selector: 'app-cafe-dashboard',
@@ -9,6 +11,7 @@ import { CafeApiService } from '../services/cafe-api.service';
 })
 export class CafeDashboardComponent implements OnInit {
 
+  private hubConnection: HubConnection;
   constructor(private _cafeApiService: CafeApiService) { }
 
   //orders: Order[];
@@ -16,9 +19,28 @@ export class CafeDashboardComponent implements OnInit {
   errMessage: string;
   ngOnInit() {
     this.onLoadOrders();
+    // this.hubConnection = new HubConnection('http://localhost:53831/signalr/hubs');
+    // this.hubConnection
+    //   .start()
+    //   .then(() => console.log('SignalR Cafe Hub Connection started!'))
+    //   .catch(err => console.log('Error while establishing connection :('));
+    // this.hubConnection.on('sendToAll',()=>{
+      
+    // });
+
   }
 
   onLoadOrders() : void{
+    this._cafeApiService
+    .getOrder()
+    .subscribe(
+      (orders)=> {
+        this.orders = orders;
+      }
+    );
+  }
+  sendToAll(): void {
+    this.hubConnection.invoke('sendToAll');
     this._cafeApiService
     .getOrder()
     .subscribe(
